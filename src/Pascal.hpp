6c5f829a997d47e557fae92b9fbbb22556ef4dea
068432 <jsd982412@gmail.com>
2024-04-19 13:19:57 -0600

    public and private keywords

--- src/Pascal.hpp
+++ src/Pascal.hpp
@@ -7,9 +7,11 @@ constexpr uint64_t len = PASCALS_TRIANGLE_DEPTH * (PASCALS_TRIANGLE_DEPTH + 1) /
 inline uint64_t getIndex(const uint64_t& n, const uint64_t& k);
 
 struct PascalsTriangle {
+public:
   PascalsTriangle();
 
   void binomial(mpz_t res, const int64_t& n, const uint64_t& k);
 
+private:
   mpz_t data[len];
 };
