f2db1887db272262f96f9314e0844b1271ccab82
068432 <jsd982412@gmail.com>
2024-04-20 12:23:59 -0600

    Poly almost done

--- src/Polynomial.cpp
+++ src/Polynomial.cpp
@@ -1,7 +1,46 @@
 #include "Polynomial.hpp"
 
 Polynomial::Polynomial(const uint64_t& num_terms_) :
+  degree(0),
   num_terms(num_terms_),
   coefficients((mpq_t*)malloc(sizeof(mpq_t) * num_terms)),
-  degrees((uint64_t*)malloc(sizeof(uint64_t) * num_terms)) {}
+  degrees((uint64_t*)malloc(sizeof(uint64_t) * num_terms)) {
+  memset(degrees, 0, sizeof(uint64_t) * num_terms);
+  for (uint64_t i = 0; i < num_terms; ++i) {
+    mpq_init(coefficients[i]);
+  }
+}
 
+void Polynomial::reInit(const uint64_t& num_terms_) {
+  for (uint64_t i = 0; i < num_terms; ++i) {
+    mpq_clear(coefficients[i]);
+  }
+  num_terms = num_terms_;
+  degree = 0;
+  free(coefficients);
+  free(degrees);
+  coefficients = (mpq_t*)malloc(sizeof(mpq_t) * num_terms);
+  degrees = (uint64_t*)malloc(sizeof(uint64_t) * num_terms);
+  memset(degrees, 0, sizeof(uint64_t) * num_terms);
+  for (uint64_t i = 0; i < num_terms; ++i) {
+    mpq_init(coefficients[i]);
+  }
+}
+
+void Polynomial::setCoefficient(const uint64_t& i, const char* str) {
+  mpq_set_str(coefficients[i], str, 0);
+  mpq_canonicalize(coefficients[i]);
+}
+
+void Polynomial::setDegree(const uint64_t& i, const uint64_t& d) {
+  degree = std::max(d, degree);
+  degrees[i] = d;
+}
+
+inline uint64_t Polynomial::getDegree() {
+  return degree;
+}
+
+void Polynomial::eval(mpq_t res, const uint64_t& x) {
+  
+}
--- src/Polynomial.hpp
+++ src/Polynomial.hpp
@@ -1,19 +1,25 @@
 #include "gmp.h"
 #include <stdint.h>
 #include <stdlib.h>
+#include <algorithm>
 
 struct Polynomial {
 public:
   Polynomial(const uint64_t& num_terms_);
 
+  void reInit(const uint64_t& num_terms_);
+
   void setCoefficient(const uint64_t& i, const char* str);
 
   void setDegree(const uint64_t& i, const uint64_t& d);
 
+  inline uint64_t getDegree();
+
   void eval(mpq_t res, const uint64_t& x);
 
 private:
-  const uint64_t num_terms;
-  mpq_t* const coefficients;
-  uint64_t* const degrees;
+  uint64_t degree;
+  uint64_t num_terms;
+  mpq_t* coefficients;
+  uint64_t*  degrees;
 };
