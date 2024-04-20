86ddd6b4000044f5c90c9ff197760645dc430a51
068432 <jsd982412@gmail.com>
2024-04-20 13:59:39 -0600

    poly eval done

--- src/Polynomial.cpp
+++ src/Polynomial.cpp
@@ -42,7 +42,43 @@ inline uint64_t Polynomial::getDegree() {
 }
 
 void Polynomial::eval(mpq_t res, const uint64_t& x) {
-  // TODO
+  mpz_t pow_x_d;
+  mpz_t numerator;
+  mpz_t denominator;
+  mpz_t new_numerator;
+  mpq_t summand;
+  mpq_t res_copy;
+  mpz_init(pow_x_d);
+  mpz_init(numerator);
+  mpz_init(denominator);
+  mpz_init(new_numerator);
+  mpq_init(summand);
+  mpq_init(res_copy);
+
+  for (uint64_t i = 0 ; i < num_terms; ++i) {
+    mpz_ui_pow_ui(pow_x_d, x, degrees[i]);
+
+    mpq_get_num(numerator, coefficients[i]);
+    mpq_get_den(denominator, coefficients[i]);
+
+    mpz_mul(new_numerator, numerator, pow_x_d);
+
+    mpq_set_num(summand, new_numerator);
+    mpq_set_den(summand, denominator);
+    mpq_canonicalize(summand);
+
+    mpq_set(res_copy, res);
+    mpq_add(res, res_copy, summand);
+
+    mpq_canonicalize(res);
+  }
+
+  mpz_clear(pow_x_d);
+  mpz_clear(numerator);
+  mpz_clear(denominator);
+  mpz_clear(new_numerator);
+  mpq_clear(summand);
+  mpq_clear(res_copy);
 }
 
 void Polynomial::destroy() {
