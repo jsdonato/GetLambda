b1218eeb78085e4325e2ac6eef099397ea53c419
068432 <jsd982412@gmail.com>
2024-12-02 23:04:31 -0700

    clean some things up

--- test/tests.hpp
+++ test/tests.hpp
@@ -2,10 +2,10 @@
 #define TESTS_HPP
 
 #include <assert.h>
-#include "../src/Pascal.hpp"
-#include "../src/Polynomial.hpp"
-#include "../src/Utility.hpp"
-#include "../src/GetLambda.hpp"
+#include "Pascal.hpp"
+#include "Polynomial.hpp"
+#include "Utility.hpp"
+#include "GetLambda.hpp"
 
 void testPascal();
 void testPolynomial();
