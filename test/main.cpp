48ad40133a290c7e7e46fcf5dcf8c7e409d1537f
068432 <jsd982412@gmail.com>
2025-08-04 18:40:50 -0400

    use define

--- test/main.cpp
+++ test/main.cpp
@@ -1,15 +1,17 @@
 #include "tests.hpp"
 
+#undef NUM_TESTS
+#define NUM_TESTS 4
+
 int main() {
-  const int num_tests = 4;
-  int tests[num_tests];
+  int tests[NUM_TESTS];
 
   tests[0] = testPascal();
   tests[1] = testPolynomial();
   tests[2] = testUtility();
   tests[3] = testGetLambda();
 
-  for (int i = 0; i < num_tests; ++i) {
+  for (int i = 0; i < NUM_TESTS; ++i) {
     if (tests[i] == EXIT_FAILURE) {
       return 1;
     }
